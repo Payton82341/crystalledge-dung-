452c739f58f2377ed6ab1acdf14da08c23a43147
Red <96445749+TheShuEd@users.noreply.github.com>
2026-02-27 00:34:55 +0300

    Juicy combat (#18)
    * fix spriteOverride
    
    * impact working
    
    * predict attack impact
    
    * finish melee attacking visuals

--- Content.Shared/_CE/Weapon/CEMeleeWeaponSystem.cs
+++ Content.Shared/_CE/Weapon/CEMeleeWeaponSystem.cs
@@ -1,26 +1,15 @@
 using System.Linq;
-using Content.Shared._CE.Camera;
 using Content.Shared.Damage.Components;
 using Content.Shared.Damage.Systems;
-using Content.Shared.Effects;
-using Robust.Shared.Player;
-using Robust.Shared.Timing;
 
 namespace Content.Shared._CE.Weapon;
 
-public sealed partial class CEMeleeWeaponSystem : EntitySystem
+public abstract class CESharedMeleeWeaponSystem : EntitySystem
 {
     [Dependency] private readonly DamageableSystem _damageable = default!;
-    [Dependency] private readonly SharedColorFlashEffectSystem _color = default!;
-    [Dependency] private readonly IGameTiming _timing = default!;
-    [Dependency] private readonly ISharedPlayerManager _player = default!;
-    [Dependency] private readonly CEScreenshakeSystem _shake = default!;
 
     public bool TryAttack(EntityUid user, Entity<CEMeleeWeaponComponent> weapon, List<EntityUid> targets)
     {
-        var userShakeRotation = new CEScreenshakeParameters() { Trauma = 0.12f, DecayRate = 1.25f, Frequency = 0.0015f };
-        var otherShakeTranslation = new CEScreenshakeParameters() { Trauma = 0.35f, DecayRate = 2f, Frequency = 0.008f };
-
         List<EntityUid> hitted = new();
         foreach (var target in targets)
         {
@@ -31,21 +20,21 @@ public sealed partial class CEMeleeWeaponSystem : EntitySystem
                 continue;
 
             hitted.Add(target);
-            _shake.Screenshake(target, otherShakeTranslation, null);
         }
 
         if (hitted.Any())
-            _shake.Screenshake(user, userShakeRotation, null);
+        {
+            RaiseAttackEffects(user, hitted);
+        }
 
-        if (_player.LocalEntity == user)
-        {
-            if (_timing.IsFirstTimePredicted)
-                _color.RaiseEffect(Color.Red, hitted, Filter.Local());
-        }
-        else
-        {
-            _color.RaiseEffect(Color.Red, hitted, Filter.Pvs(user, entityManager: EntityManager).RemoveWhereAttachedEntity(o => o == user));
-        }
         return true;
+    }
+
+    /// <summary>
+    /// Override this method in client/server implementations to handle visual effects.
+    /// </summary>
+    protected virtual void RaiseAttackEffects(EntityUid user, List<EntityUid> targets)
+    {
+        // Base implementation does nothing - effects are handled in client/server implementations
     }
 }
